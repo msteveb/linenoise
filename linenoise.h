9bafa6c1939f2d61f774ef2b1ee7aec4d98f195c
Stephan Beal <sgbeal@googlemail.com>
2014-05-14 17:32:22 +0200

    Code review doc fixes from Steve B.

--- linenoise.h
+++ linenoise.h
@@ -49,12 +49,14 @@ typedef struct linenoiseCompletions {
 typedef void(linenoiseCompletionCallback)(const char *, linenoiseCompletions *);
 
 /*
- * Sets the current tab completion handler and returns the previous one.
+ * Sets the current tab completion handler and returns the previous one, or NULL
+ * if no prior one has been set.
  */
 linenoiseCompletionCallback * linenoiseSetCompletionCallback(linenoiseCompletionCallback *);
 
 /*
- * Adds a copy of the given string to the list of default completions.
+ * Adds a copy of the given string to the given completion list. The copy is owned
+ * by the linenoiseCompletions object.
  */
 void linenoiseAddCompletion(linenoiseCompletions *, const char *);
 #endif
@@ -62,7 +64,7 @@ void linenoiseAddCompletion(linenoiseCompletions *, const char *);
 /*
  * Prompts for input using the given string as the input
  * prompt. Returns when the user has tapped ENTER or (on an empty
- * line) EOF (Ctrl-D on Unix, Ctrl-Z on Windows) on an. Returns either
+ * line) EOF (Ctrl-D on Unix, Ctrl-Z on Windows). Returns either
  * a copy of the entered string (for ENTER) or NULL (on EOF).  The
  * caller owns the returned string and must eventually free() it.
  */
@@ -76,7 +78,7 @@ int linenoiseHistoryAdd(const char *line);
 /*
  * Sets the maximum length of the command history, in lines.
  * If the history is currently longer, it will be trimmed,
- * retaining on the most recent entries. If len is 0 or less
+ * retaining only the most recent entries. If len is 0 or less
  * then this function does nothing.
  */
 int linenoiseHistorySetMaxLen(int len);
@@ -99,7 +101,7 @@ int linenoiseHistorySave(const char *filename);
 int linenoiseHistoryLoad(const char *filename);
 
 /*
- * Frees all history entries, clearning the history.
+ * Frees all history entries, clearing the history.
  */
 void linenoiseHistoryFree(void);
 
