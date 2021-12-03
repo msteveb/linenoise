d2fb16a95959147f19794255eb87e303528eddc2
David Woodhouse <dwmw@amazon.co.uk>
2021-12-03 13:22:57 +0000

    Echo input when reading from non-tty
    Signed-off-by: David Woodhouse <dwmw@amazon.co.uk>

--- linenoise.c
+++ linenoise.c
@@ -1878,6 +1878,10 @@ char *linenoiseWithInitial(const char *prompt, const char *initial)
         printf("%s", prompt);
         fflush(stdout);
         sb = sb_getline(stdin);
+        if (!isatty(current.fd) && sb) {
+            printf("%s\n", sb_str(sb));
+            fflush(stdout);
+        }
     }
     else {
         current.buf = sb_alloc();
